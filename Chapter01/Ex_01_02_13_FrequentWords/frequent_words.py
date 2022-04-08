dcb8abec2ef41705a67866b7d0adc662167c3835
Ben De Jonge (imec) <74603987+BenDeJonge@users.noreply.github.com>
2022-04-08 02:03:43 +0200

    Update frequent_words.py
    Split into a separate frequency_table function.

--- Chapter01/Ex_01_02_13_FrequentWords/frequent_words.py
+++ Chapter01/Ex_01_02_13_FrequentWords/frequent_words.py
@@ -7,6 +7,35 @@ Created on Wed Apr  6 09:05:05 2022
 
 #==============================================================================
 
+def get_frequency_table(text : str, k : int) -> dict:
+    '''
+    Given a text (string) and a size k (int), return the a frequency table of
+    occurences of text fragment of length k in the text.
+    
+    Parameters
+    ----------
+    text : str
+        The text to look for patterns.
+    k : int
+        The length of the pattern to look for.
+
+    Returns
+    -------
+    dict
+        A frequency table of occurences of fragments of length k inside text.
+    '''
+    # Construct frequency table.
+    frequent_words = {}
+    for i in range(len(text) - k + 1):
+        fragment = text[ i : i + k ]
+        try:
+            frequent_words[fragment] += 1
+        except KeyError:
+            frequent_words[fragment] = 1
+    return frequent_words
+
+#==============================================================================
+
 def frequent_words(text : str, k : int) -> list:
     '''
     Given a text (string) and a size k (int), return the text fragment of 
@@ -27,17 +56,10 @@ def frequent_words(text : str, k : int) -> list:
     list
         The fragment of length k with the most occurences.
     '''
-    # Construct frequency table.
-    frequent_words = {}
-    for i in range(len(text) - k + 1):
-        fragment = text[ i : i + k ]
-        try:
-            frequent_words[fragment] += 1
-        except KeyError:
-            frequent_words[fragment] = 1
+    fw = get_frequency_table(text, k)
     # Find max frequency and get all values with that frequency.
-    max_count = max(frequent_words.values())
-    frequent_list = [ fragment for fragment, count in frequent_words.items() 
+    max_count = max(fw.values())
+    frequent_list = [ fragment for fragment, count in fw.items() 
                       if count == max_count ]   
     return sorted(frequent_list)
 
