9909350b8175d2512828e8b561a3fabb93844d21
Ben De Jonge (imec) <74603987+BenDeJonge@users.noreply.github.com>
2022-04-15 00:23:06 +0200

    Added documentation.

--- Chapter01/Ex_01_07_08_SkewDiagram/skew_diagram.py
+++ Chapter01/Ex_01_07_08_SkewDiagram/skew_diagram.py
@@ -5,7 +5,20 @@ Created on Fri Apr  8 15:41:38 2022
 @author: dejong71
 """
 
-def skew_diagram(genome):
+def skew_diagram(genome : str) -> list[int]:
+    '''
+    Given a genome, calculate the GC-skew along the whole sequence.
+
+    Parameters
+    ----------
+    genome : str
+        The sequence for which to calculate the GC-skew.
+
+    Returns
+    -------
+    list[int]
+        The current GC-skew at each individual index.
+    '''
     skews = [0]
     for base in genome:
         if base == 'C':
